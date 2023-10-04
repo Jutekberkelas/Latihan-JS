f70bc1ce339122f4a3f4e2e0b88ce5ad7f089ea9
Jutekberkelas <114629808+Jutekberkelas@users.noreply.github.com>
2023-10-04 10:42:36 +0700

    Add files via upload

---
+++ Latihan.js
@@ -0,0 +1,11 @@
+var firstName = "Mirna"; 
+var lastName = "Kamilah";
+var fullName = "Mirna Kamilah"
+var age = 19; 
+var isMarried = false;
+
+console.log(firstName)
+console.log(lastName) 
+console.log(fullName)
+console.log(age)
+console.log(isMarried) 
